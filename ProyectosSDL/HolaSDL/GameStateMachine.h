363e8549fd208b5e282a893c0ada4bc43bd7bacc
L <leonorcu@ucm.es>
2018-01-16 22:39:27 +0100

    Añadida clase GameStateMachine con todos los include, creo

---
+++ ProyectosSDL/HolaSDL/GameStateMachine.h
@@ -0,0 +1,13 @@
+#pragma once
+#include <stack>
+#include "EndState.h"
+#include "MainMenuState.h"
+#include "PauseState.h"
+
+class GameStateMachine
+{
+public:
+	GameStateMachine();
+	~GameStateMachine();
+};
+
